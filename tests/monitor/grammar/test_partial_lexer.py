b8ab117b1aa8378ab1cb53c7028d8f01f6ce2ba3
Kanghee Park <kpark247@wisc.edu>
2024-11-13 17:33:24 -0800

    Consider priority at lexing

--- tests/monitor/grammar/test_partial_lexer.py
+++ tests/monitor/grammar/test_partial_lexer.py
@@ -48,3 +48,57 @@ class PartialLexerFSTTest(TestCase):
 
         _, out = fst.follow(state_1, vocabulary['---'])
         self.assertEqual(len(out), 1)
+
+    def test_if_grammar(self):
+        grammar = """
+            ?start: sum
+                | NAME "=" sum    
+
+            ?sum: product
+                | sum "+" product   
+                | sum "-" product   
+                | sum "---" product 
+
+            ?product: atom
+                | product "*" atom  
+                | product "/" atom 
+
+            ?atom: NUMBER           
+                | "-" atom
+                | IF       
+                | NAME            
+                | "(" sum ")"
+
+            
+            IF.0: "if"
+            NAME.1: CNAME
+
+            %import common.CNAME
+            %import common.NUMBER
+            %import common.WS_INLINE
+
+            %ignore WS_INLINE
+        """
+
+        lexer_conf = Lark(grammar, parser='lalr').lexer_conf
+        vocabulary = {'if ':1, 'iff':2, 'if':3, ' ':4}
+
+        fst = PartialLexerFST(lexer_conf, vocabulary)
+
+        state_1, out = fst.follow(fst.initial, vocabulary['iff'])
+        self.assertEqual(len(out), 0)
+
+        _, out = fst.follow(state_1, vocabulary[' '])
+        self.assertEqual(len(out), 1)
+
+        state_1, out = fst.follow(fst.initial, vocabulary['if'])
+        self.assertEqual(len(out), 0)
+
+        _, out = fst.follow(state_1, vocabulary['iff'])
+        self.assertEqual(len(out), 0)
+
+        _, out = fst.follow(state_1, vocabulary[' '])
+        self.assertEqual(len(out), 1)
+
+        print([(t.name, t.priority) for t in lexer_conf.terminals])
+        self.assertEqual(out[0].name, 'IF')
